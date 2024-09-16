0f4711a9ccc47c7b51fba4320489010de0fdec28
Paradon Srita <paradonsrita2003@gmail.com>
2024-09-16 09:51:58 +0700

    before-presentation-on-16-9-67

--- obj/Debug/net7.0/QMS.AssemblyInfo.cs
+++ obj/Debug/net7.0/QMS.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QMS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b116647460abbba0764c7a2d77fa9e9c53d88a1e")]
 [assembly: System.Reflection.AssemblyProductAttribute("QMS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QMS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
